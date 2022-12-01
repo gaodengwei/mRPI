d0340b8513c74a59c77ee6b50c47412e8dd1878b
gaodengwei <60092207+gaodengwei@users.noreply.github.com>
2022-12-01 22:55:07 +0800

    Add files via upload

--- mRPI_DT.m
+++ mRPI_DT.m
@@ -26,11 +26,12 @@ switch options.methods
         if (~isfield(options,'max_iterations')), options.max_iterations=100; end
         if (~isfield(options,'converged_tol')), options.converged_tol=.01; end
         if (~isfield(options,'degL')), options.degL = 4; end
+        if (~isfield(options,'backoff')), options.backoff = 0; end 
         
         gw = [w-prob.blc(dimw);prob.buc(dimw)-w]; 
         gx = [x-prob.blc(dimx);prob.buc(dimx)-x;]; 
-        gu = [-K*x-prob.blc(dimu);prob.buc(dimu)+K*x;]; 
         if prob.feedback
+            gu = [-K*x-prob.blc(dimu);prob.buc(dimu)+K*x;]; 
             gx = [gx;gu]; 
         end
         % add feedback
@@ -51,7 +52,7 @@ switch options.methods
         gx = [x-prob.blc(dimx);prob.buc(dimx)-x;];
         gu = [u-prob.blc(dimu);prob.buc(dimu)-u;]; 
         f = prob.sys;
-        uf = -K*x;
+        uf = clean(-K*x);
         [Phi,u]  = mRPI_DT_optcontrol(x,w,u,f,gw,gx,gu,uf,Phi,options); 
         
         varargout{1} = Phi; 
@@ -62,7 +63,11 @@ switch options.methods
         B = double(diff(f,u));  
         wuc = prob.buc(dimw); wlc = prob.blc(dimw);
         Wbound = [wuc;wlc];
-        W_vertex = [Wbound,Wbound([2,3,4,1])];
+        n = length(Wbound);
+        W_vertex = Wbound;
+        for i=1:n/2-1
+            W_vertex = [W_vertex,Wbound([i+1:n,1:i])];
+        end
 %         W_vertex = [0.1, 0.1; 0.1, -0.1; -0.1, -0.1; -0.1, 0.1];
         y = RPIsolveMinkowski(A,B,K,W_vertex); 
         varargout{1} = y;
@@ -75,19 +80,18 @@ function [Phi,iterVOL] = RPIsolveSOS(x,w,f,gw,gx,Phi,options)
 
 Lmonom = monomials([x;w],0:options.degL);
 vol = det(Phi);
-iterVOL = -log(sqrt(vol));
-rho = 1;
-iterR = rho; 
-t_opt=1;
+iterVOL = -log(sqrt(vol)); 
+iterV = vol;  
 hhh=figure;
 
 for i=1:options.max_iterations
     last_vol = vol;
-    [L,rho] = FindL(x,w,f,Phi,gw,gx,Lmonom);
-    Phi = FindV(x,w,f,L,gw,gx,rho,Phi/rho,Lmonom); 
-    iterR = [iterR,rho];  
+    [L,rho] = FindL(x,w,f,Phi,gw,gx,Lmonom,options);
+    Phi = FindV(x,w,f,L,gw,gx,rho,Phi/rho,Lmonom,options); %*options.backoff 
     vol = det(Phi);
+    iterV = [iterV,vol]; 
     % check for convergence
+    disp([num2str(vol - last_vol),'<',num2str(options.converged_tol*last_vol )])
     if (vol - last_vol) < options.converged_tol*last_vol 
         delete(hhh)
         break;
@@ -100,7 +104,7 @@ end
 
 end
 
-function [L,rho] = FindL(x,w,f,Phi,gw,gx,Lmonom)
+function [L,rho] = FindL(x,w,f,Phi,gw,gx,Lmonom,options)
 
 prog = spotsosprog;
 prog = prog.withIndeterminate(x);
@@ -118,36 +122,40 @@ p2 = f'*Phi*f;
 prog = prog.withSOS(L);
 prog = prog.withSOS(Lw);
 prog = prog.withSOS(Lx);
-prog = prog.withSOS(rho-p2-L*(1-p1)-Lw'*gw-Lx'*gx);%-gama*p1
-
+prog = prog.withSOS(rho-p2-L*(1-p1)-Lw'*gw-Lx'*gx);%-gama*p1 
 % solve SOS
 solver = @spot_mosek;%spot_gurobi;%
 pars = spot_sdp_default_options();
 pars.verbose = 0;
+costfun = rho;
 sol = prog.minimize(rho,solver,pars);  %　no optimaize obj
 
-L = sol.eval(L);
-% Lw = sol.eval(Lw);
-% Lx = sol.eval(Lx);
+% back off
+if options.backoff~=0
+    costfun_opt = double(sol.eval(costfun));
+    prog = prog.withPos(costfun_opt + (options.backoff)*abs(costfun_opt) - costfun);
+    sol = prog.minimize(0,@spot_mosek,pars);
+end
+L = sol.eval(L); 
 rho = double(sol.eval(rho)); 
+
 
 end
 
 
-function Phi = FindV(x,w,xp,L,gw,gx,rho,Phi,Lmonom)
+function Phi = FindV(x,w,xp,L,gw,gx,rho,Phi,Lmonom,options)
 
 prog = spotsosprog;
 prog = prog.withIndeterminate(x);
 prog = prog.withIndeterminate(w);
 
-[prog,P] = prog.newPSD(2);
+[prog,P] = prog.newPSD(length(x));
 
 p1 = x'*P*x;
 p2 = xp'*P*xp;
 
 [prog,Lw] = prog.newFreePoly(Lmonom,length(gw));
-[prog,Lx] = prog.newFreePoly(Lmonom,length(gx));
-% [prog,L0] = prog.newFreePoly(Lmonom);
+[prog,Lx] = prog.newFreePoly(Lmonom,length(gx)); 
 
 % setup SOS constraints
 prog = prog.withSOS(Lw);
@@ -157,9 +165,16 @@ prog = prog.withSOS(1-p2-L*(1-p1)-Lw'*gw-Lx'*gx);
 solver = @spot_mosek;
 pars = spot_sdp_default_options();
 pars.verbose = 0;
-sol = prog.minimize(-trace(Phi^-1*P),solver,pars); %-det(P) replaced by -trace(Phi^-1*P)
-% [prog,t] = maxdet(prog,P);
-% sol = prog.minimize(-t,solver,pars);
+costfun = -trace(Phi^-1*P);
+sol = prog.minimize(costfun,solver,pars); %-det(P) replaced by -trace(Phi^-1*P)
+
+% back off
+if options.backoff~=0
+    costfun_opt = double(sol.eval(costfun));
+    prog = prog.withPos(costfun_opt + (options.backoff)*abs(costfun_opt) - costfun);
+    sol = prog.minimize(0,@spot_mosek,pars);
+end
+ 
 if ~sol.isDualFeasible 
     keyboard
 end
@@ -173,25 +188,38 @@ function [Phi,uf]  = mRPI_DT_optcontrol(x,w,u,f,gw,gx,gu,uf,Phi,options)
   % ==========begin ===================================
 Lmonom = monomials([x;w],0:options.degL);
 Lmom = monomials(x,0:options.degL);
-um = monomials(x,1:options.degU);
+um = monomials(x,1:options.degU); 
 vol = det(Phi);
-iterVOL = -log(sqrt(vol));  
+iterVOL = -log(sqrt(vol)); 
+iterV = vol;  
+hhh=figure;
 
 for i=1:options.max_iterations
     last_vol = vol;
-    [L,Lu,rho] = FindLU(x,u,w,f,Phi,gw,gx,gu,Lmonom,Lmom,um,uf);
-    [uf,rho] = FindU(x,u,w,f,L,Lu,Phi/rho,gw,gx,gu,Lmonom,um,uf);
-    Phi = FindVU(x,u,w,f,L,gw,gx,Phi/rho,uf,Lmonom);
+    [L,Lu,rho] = FindLU(x,u,w,f,Phi,gw,gx,gu,Lmonom,Lmom,um,uf,options);
+    [uf,rho] = FindU(x,u,w,f,L,Lu,Phi,gw,gx,gu,Lmonom,um,uf,options);
+    Phi = FindVU(x,u,w,f,L,Lu,gw,gx,Phi/rho,uf,gu,Lmonom,options);
     vol = det(Phi);
+    iterV = [iterV,vol]; 
     % check for convergence
-    if ((vol - last_vol) < options.converged_tol*last_vol)
+    disp([num2str(vol - last_vol),'<',num2str(options.converged_tol*last_vol )])
+    if (vol - last_vol) < options.converged_tol*last_vol 
+        delete(hhh)
         break;
     end
-    i
+    preP = Phi;
     iterVOL = [iterVOL,-log(sqrt(vol))];
+    plot(iterVOL,'k-*')
+    drawnow
+    Phi
+    uf
 end 
 end
-function [L,Lu,rho] = FindLU(x,u,w,f,Phi,gw,gx,gu,Lmonom,Lmom,um,u0)
+
+
+
+
+function [L,Lu,rho] = FindLU(x,u,w,f,Phi,gw,gx,gu,Lmonom,Lmom,um,u0,options)
  
 prog = spotsosprog;
 prog = prog.withIndeterminate(x);
@@ -207,29 +235,35 @@ end
 
 [prog,L] = prog.newFreePoly(Lmonom);  
 [prog,Lw] = prog.newFreePoly(Lmonom,length(gw));
-% [prog,Lx] = prog.newFreePoly(Lmonom,length(gx));
-  
+[prog,Lx] = prog.newFreePoly(Lmonom,length(gx)); 
 [prog,Lu] = prog.newFreePoly(Lmom,length(gu));
 
-B = diff(f,u);
+B = options.B;
 p1 = x'*Phi*x;
-p2 = f'*Phi*f - u'*B'*Phi*B*u + u0'*B'*Phi*B*uf;  %; + u0'*B'*Phi*B*uf;
-p2 = subs(p2,u,uf);  
+p2 = f'*Phi*f + 2*f'*Phi*B*uf + u0'*B'*Phi*B*uf;  
 
 % setup SOS constraints
 prog = prog.withSOS(L); 
-prog = prog.withSOS(Lw);
-% prog = prog.withSOS(Lx); 
+prog = prog.withSOS(Lw); 
+prog = prog.withSOS(Lx);
 prog = prog.withSOS(Lu);
-prog = prog.withSOS(rho-p2-L*(1-p1)-Lw'*gw); %-Lx'*gx
+prog = prog.withSOS(1-p2-L*(1-p1)-Lw'*gw-Lx'*gx); 
+% prog = prog.withPos(rho-0.9); % max 80% desect
 Gu = subs(gu,u,uf);  
-prog = prog.withSOS(Gu-Lu*(1-p1));
- 
+prog = prog.withSOS(Gu-Lu*(1-p1)); 
 % solve SOS
 solver = @spot_mosek;%spot_gurobi;%
 pars = spot_sdp_default_options();
 pars.verbose = 0;
-sol = prog.minimize(rho,solver,pars);  %　no optimaize obj
+costfun = 0;
+sol = prog.minimize(costfun,solver,pars);  
+
+% back off
+% if options.backoff~=0
+%     costfun_opt = double(sol.eval(costfun));
+%     prog = prog.withPos(costfun_opt + (options.backoff)*abs(costfun_opt) - costfun);
+%     sol = prog.minimize(0,@spot_mosek,pars);
+% end
 
 L = sol.eval(L);  
 Lu = sol.eval(Lu);
@@ -238,7 +272,7 @@ rho = double(sol.eval(rho));
  
 end
 
-function [uf,rho] = FindU(x,u,w,f,L,Lu,Phi,gw,gx,gu,Lmonom,um,u0) 
+function [uf,rho] = FindU(x,u,w,f,L,Lu,Phi,gw,gx,gu,Lmonom,um,u0,options) 
 prog = spotsosprog;
 prog = prog.withIndeterminate(x);
 prog = prog.withIndeterminate(w);
@@ -252,57 +286,73 @@ for j = 1:size(u,1)
 end
  
 [prog,Lw] = prog.newFreePoly(Lmonom,length(gw));
-% [prog,Lx] = prog.newFreePoly(Lmonom,length(gx));
+[prog,Lx] = prog.newFreePoly(Lmonom,length(gx));
   
-B = diff(f,u);
+B = options.B;
 p1 = x'*Phi*x;
-p2 = f'*Phi*f - u'*B'*Phi*B*u + u0'*B'*Phi*B*uf;  %;
-p2 = subs(p2,u,uf);  
+p2 = f'*Phi*f + 2*f'*Phi*B'*uf + u0'*B'*Phi*B*uf;    
 
 % setup SOS constraints 
 prog = prog.withSOS(Lw);
-% prog = prog.withSOS(Lx);  
-prog = prog.withSOS(rho-p2-L*(rho-p1)-Lw'*gw); %-Lx'*gx
+prog = prog.withSOS(Lx);  
+prog = prog.withSOS(rho-p2-L*(rho-p1)-Lw'*gw-Lx'*gx); %
+prog = prog.withPos(rho-0.8); % max 80% desect
 Gu = subs(gu,u,uf);  
-prog = prog.withSOS(Gu-Lu*(rho-p1));
- 
+prog = prog.withSOS(Gu-Lu*(rho-p1)); 
 % solve SOS
 solver = @spot_mosek;%spot_gurobi;%
 pars = spot_sdp_default_options();
 pars.verbose = 0;
-sol = prog.minimize(rho,solver,pars);  %　no optimaize obj
+costfun = rho;
+sol = prog.minimize(costfun,solver,pars); 
+
+% back off
+if options.backoff~=0
+    costfun_opt = double(sol.eval(costfun));
+    prog = prog.withPos(costfun_opt + (options.backoff)*abs(costfun_opt) - costfun);
+    sol = prog.minimize(0,@spot_mosek,pars);
+end
  
 rho = double(sol.eval(rho));
-uf = sol.eval(uf);
+uf = clean(sol.eval(uf));
  
 end
 
-function Phi = FindVU(x,u,w,f,L,gw,gx,Phi,uf,Lmonom)
+function Phi = FindVU(x,u,w,f,L,Lu,gw,gx,Phi,uf,gu,Lmonom,options)
 prog = spotsosprog;
 prog = prog.withIndeterminate(x);
 prog = prog.withIndeterminate(w);
 
-[prog,P] = prog.newPSD(2); 
+[prog,P] = prog.newPSD(length(x)); 
 [prog,Lw] = prog.newFreePoly(Lmonom,length(gw));
-% [prog,Lx] = prog.newFreePoly(Lmonom,length(gx));
+[prog,Lx] = prog.newFreePoly(Lmonom,length(gx));
 
-B = diff(f,u);
+B = options.B;
 p1 = x'*P*x;
-p2 = f'*P*f- u'*B'*P*B*u + uf'*B'*Phi*B*uf;  % - u'*B'*P*B*u
-p2 = subs(p2,u,uf); 
+p2 = f'*P*f + 2*f'*P*B'*uf + uf'*B'*P*B*uf;    
 
 % setup SOS constraints
 prog = prog.withSOS(Lw);
-% prog = prog.withSOS(Lx); 
-prog = prog.withSOS(1-p2-L*(1-p1)-Lw'*gw);%-Lx'*gx
-% Gu = subs(gu,u,uf);
-% prog = prog.withSOS(Gu-Lu*(1-p1));
+prog = prog.withSOS(Lx); 
+prog = prog.withSOS(1-p2-L*(1-p1)-Lw'*gw-Lx'*gx );%
+Gu = subs(gu,u,uf);  
+prog = prog.withSOS(Gu-Lu*(1-p1));
 % solve SOS
 solver = @spot_mosek;
 pars = spot_sdp_default_options();
 pars.verbose = 0;  
- 
-sol = prog.minimize(-trace(Phi^-1*P),solver,pars);
+costfun = trace(Phi^-1*P);
+% [prog,tauk] = maxdet(prog,P); 
+% costfun = -tauk; 
+sol = prog.minimize(costfun,solver,pars);
+
+% back off
+if options.backoff~=0
+    costfun_opt = double(sol.eval(costfun));
+    prog = prog.withPos(costfun_opt + (options.backoff)*abs(costfun_opt) - costfun);
+    sol = prog.minimize(0,@spot_mosek,pars);
+end
+
 if ~sol.isDualFeasible 
     keyboard
 end
